f0d0aa406eaee2c6c9d9221446a861e1de4924eb
Matthew Yeow <myeow@u81z05s04.42kl.edu.my>
2024-07-19 17:38:01 +0800

    Draw simple square as start

--- srcs/FdF/FdF.c
+++ srcs/FdF/FdF.c
@@ -6,18 +6,39 @@
 /*   By: myeow <myeow@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/19 16:22:23 by myeow             #+#    #+#             */
-/*   Updated: 2024/07/19 16:24:44 by myeow            ###   ########.fr       */
+/*   Updated: 2024/07/19 17:21:39 by myeow            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <mlx.h>
+#include "FdF.h"
+
+static void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*((unsigned int *) dst) = color;
+}
 
 int	main(void)
 {
 	void	*mlx;
 	void	*mlx_win;
+	t_data	img;
+	int		i = 500;
+	int		j = 500;
 
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello World!");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, \
+			&img.line_length, &img.endian);
+	while (++i < 1000)
+	{
+		j = 500;
+		while(++j < 800)
+			my_mlx_pixel_put(&img, i, j, 0x00FF0000);
+	}
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 }
