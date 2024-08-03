789f43a6dab13b1c675cf6e02a13d2e0e98e498d
Matthew Yeow <matthewyeow.yeow@gmail.com>
2024-08-03 22:32:11 +0800

    Drew circle

--- srcs/FdF/FdF_draw_line.c
+++ srcs/FdF/FdF_draw_line.c
@@ -6,7 +6,7 @@
 /*   By: myeow <myeow@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/30 19:52:34 by myeow             #+#    #+#             */
-/*   Updated: 2024/07/31 00:46:00 by myeow            ###   ########.fr       */
+/*   Updated: 2024/08/03 22:30:37 by myeow            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,11 @@
 
 static void	plot(t_data *data, int x, int y, double intensity)
 {
-	t_color	color;
-	
-	printf("x: %d, y: %d\n", x, y);
-	color = (t_color) {.t_trgb.r = 255, \
-		.t_trgb.t = (unsigned char)(255 * intensity)};
-	mlx_plot_pixel(data, x, y, color);
+	t_color	temp_color;
+
+	temp_color = data->curr_color;
+	temp_color.t_trgb.t = (unsigned char)(255 * (1 - intensity));
+	mlx_plot_pixel(data, x, y, temp_color);
 }
 
 static double	first_endpoint_plot(t_data *data, t_draw_line *info)
@@ -70,7 +69,7 @@ static void	second_endpoint_plot(t_data *data, t_draw_line *info)
 	}
 }
 
-static void	FdF_draw_line_helper(t_data *data, t_draw_line *info)
+static void	fdf_draw_line_helper(t_data *data, t_draw_line *info)
 {
 	double	intery;
 	int		x;
@@ -85,7 +84,6 @@ static void	FdF_draw_line_helper(t_data *data, t_draw_line *info)
 			plot(data, (int)intery, x, ft_rev_fractional(intery));
 			plot(data, (int)intery + 1, x, ft_fractional(intery));
 			intery += info->gradient;
-			printf("%f\n", info->gradient);
 		}
 	}
 	else
@@ -99,15 +97,16 @@ static void	FdF_draw_line_helper(t_data *data, t_draw_line *info)
 	}
 }
 
-void	FdF_draw_line(t_data *data, t_vec2 *a, t_vec2 *b)
+void	fdf_draw_line(t_data *data, t_vec2 *a, t_vec2 *b)
 {
 	t_draw_line	info;
+	int			dx;
 
 	info.x0 = a->x;
 	info.y0 = a->y;
 	info.x1 = b->x;
 	info.y1 = b->y;
-	info.steep = ft_abs(info.y1 - info.y0) > ft_abs(info.x1 - info.x0);
+	info.steep = ft_abs(info.y1 - info.y0) >= ft_abs(info.x1 - info.x0);
 	if (info.steep)
 	{
 		ft_swap(&info.x0, &info.y0);
@@ -118,6 +117,10 @@ void	FdF_draw_line(t_data *data, t_vec2 *a, t_vec2 *b)
 		ft_swap(&info.x0, &info.x1);
 		ft_swap(&info.y0, &info.y1);
 	}
-	info.gradient = (info.y1 - info.y0) / (info.x1 - info.x0);
-	FdF_draw_line_helper(data, &info);
+	dx = info.x1 - info.x0;
+	if (!dx)
+		info.gradient = 1;
+	else
+		info.gradient = (info.y1 - info.y0) / dx;
+	fdf_draw_line_helper(data, &info);
 }
